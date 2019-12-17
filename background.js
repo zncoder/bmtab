2df44c408d37a8ba38e9a0574483df548541f123
bin zhang <binz@uber.com>
2019-12-17 12:23:38 -0800

    async

--- background.js
+++ background.js
@@ -7,28 +7,26 @@ let folderId
 let historyId
 
 function showBmtabPage() {
-	chrome.tabs.create({"url": "/page.html"})
+	return tabDo("create", {"url": "/page.html"})
 }
 
-function captureTab() {
-	chrome.tabs.query(
-		{currentWindow: true, active: true},
-		tabs => {
-			let tab = tabs[0]
-			if (specialPage(tab.url)) {
-				return
-			}
+async function currentTab() {
+	let tabs = await tabDo("query", {currentWindow: true, active: true})
+	return tabs[0]
+}
 
-			dedup(folderId, tab.url)
-				.then(() => {
-					let arg = {parentId: folderId, index: 0, title: tab.title, url: tab.url}
-					bmDo("create", arg)
-						.then(item => {
-							chrome.tabs.remove(tab.id)
-							buildMenus()
-						})
-				})
-		})
+async function captureTab() {
+	let tab = await currentTab()
+	if (specialPage(tab.url)) {
+		return
+	}
+
+	await dedup(folderId, tab.url)
+
+	let arg = {parentId: folderId, index: 0, title: tab.title, url: tab.url}
+	await bmDo("create", arg)
+	await tabDo("remove", tab.id)
+	return buildMenus()
 }
 
 function specialPage(url) {
@@ -37,51 +35,42 @@ function specialPage(url) {
 
 function restoreTab(tab, url) {
 	if (specialPage(tab.url)) {
-		chrome.tabs.update(tab.id, {url: url})
+		return tabDo("update", tab.id, {url: url})
 	} else {
-		chrome.tabs.create({url: url, active: true})
+		return tabDo("create", {url: url, active: true})
 	}
 }
 
-function initBmtab() {
+async function initBmtab() {
 	// create bmtab and history bookmark folder
-	return createBmtabFolder()
-		.then(id => {
-			folderId = id
-			return createHistoryFolder()
-		})
-		.then(id => historyId = id)
+	folderId = await createBmtabFolder()
+	historyId = await createHistoryFolder()
 }
 
-function createBmtabFolder() {
-	return bmDo("search", {title: bmtabTitle})
-		.then(items => {
-			if (items.length > 0) {
-				return items[0]
-			}
-			return bmDo("create", {title: bmtabTitle})
-		})
-		.then(item => item.id)
+async function createBmtabFolder() {
+	let items = await bmDo("search", {title: bmtabTitle})
+	if (items.length > 0) {
+		return items[0].id
+	}
+	let item = await bmDo("create", {title: bmtabTitle})
+	return item.id
 }
 
-function createHistoryFolder() {
-	return bmDo("getChildren", folderId)
-		.then(items => {
-			for (let x of items) {
-				if (x.title === historyTitle) {
-					return x
-				}
-			}
-
-			let arg = {title: historyTitle, parentId: folderId}
-			return bmDo("create", arg)
-		})
-		.then(item => item.id)
+async function createHistoryFolder() {
+	let items = await bmDo("getChildren", folderId)
+	for (let x of items) {
+		if (x.title === historyTitle) {
+			return x.id
+		}
+	}
+	let arg = {title: historyTitle, parentId: folderId}
+	let item = await bmDo("create", arg)
+	return item.id
 }
 
-function getBmtabs() {
-	return bmDo("getChildren", folderId)
-		.then(items => items.filter(x => x.id !== historyId))
+async function getBmtabs() {
+	let items = await bmDo("getChildren", folderId)
+	return items.filter(x => x.id !== historyId)
 }
 
 function removeMenus() {
@@ -96,74 +85,64 @@ function newMenu(arg) {
 	})
 }
 
-function removeBookmark(id) {
-	return bmDo("get", id)
-		.then(items => {
-			if (items.length == 0) {
-				return
-			}
+async function removeBookmark(id) {
+	let items = await bmDo("get", id)
+	if (items.length == 0) {
+		return
+	}
 
-			let arg = {parentId: historyId, index: 0, title: items[0].title, url: items[0].url}
-			return dedup(historyId, arg.url)
-				.then(() => bmDo("create", arg))
-				.then(() => bmDo("remove", id))
-		})
+	let arg = {parentId: historyId, index: 0, title: items[0].title, url: items[0].url}
+	await dedup(historyId, arg.url)
+	await bmDo("create", arg)
+	return bmDo("remove", id)
 }
 
-function dedup(folder, url) {
-	return bmDo("getChildren", folder)
-		.then(items => {
-			return items
-				.filter(x => x.url === url)
-				.map(x => bmDo("remove", x.id))
-		})
-		.then(ps => Promise.all(ps))
+async function dedup(folder, url) {
+	let items = await bmDo("getChildren", folder)
+	return Promise.all(
+		items
+			.filter(x => x.url === url)
+			.map(x => bmDo("remove", x.id)))
 }
 
-function handleMenu(info, tab) {
-	getBmtabs()
-		.then(items => {
-			let id = parseInt(info.menuItemId)
-			//console.log("handle menu:" + id + " of items"); console.log(items)
-			if (id === menuIdPage) {
-				showBmtabPage()
-				return
-			}
+async function handleMenu(info, tab) {
+	let items = await getBmtabs()
+	let id = parseInt(info.menuItemId)
+	//console.log("handle menu:" + id + " of items"); console.log(items)
+	if (id === menuIdPage) {
+		await showBmtabPage()
+	} else {	
+		id -= menuIdBase
+		if (id >= items.length) {
+			console.log("menuitemid:" + (id+menuIdBase) + " out of bound")
+			return
+		}
+		let it = items[id]
+		//console.log("show item:" + it.id + ",url:" + it.url)
+		await restoreTab(tab, it.url)
+		await removeBookmark(it.id)
+	}
+	return buildMenus()
+}
+
+async function buildMenus() {
+	await removeMenus()
 	
-			id -= menuIdBase
-			if (id >= items.length) {
-				console.log("menuitemid:" + (id+menuIdBase) + " out of bound")
-				return
-			}
-			let it = items[id]
-			//console.log("show item:" + it.id + ",url:" + it.url)
-			restoreTab(tab, it.url)
-			return removeBookmark(it.id)
-		})
-		.then(buildMenus)
-}
+	let items = await getBmtabs()
+	//console.log("buildmenus items"); console.log(items)
+	let max = items.length
+	let title = "* Bmtab Page *"
+	if (max - 1 > chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT) {
+		max = chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT - 1
+		title = "* Bmtab Page [...] *"
+	}
 
-function buildMenus() {
-	let items
-	return removeMenus()
-		.then(getBmtabs)
-		.then(x => {
-			items = x
-			//console.log("buildmenus items"); console.log(items)
-		})
-		.then(() => {
-			let max = items.length
-			let title = "* Bmtab Page *"
-			if (max - 1 > chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT) {
-				max = chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT - 1
-				title = "* Bmtab Page [...] *"
-			}
-			
-			newMenu({id: menuIdPage + "", title: title})
-			for (let i = 0; i < max; i++) {
-				newMenu({id: menuIdBase + i + "", title: items[i].title})
-			}
-		})
+	let ps = []
+	ps.push(newMenu({id: menuIdPage + "", title: title}))
+	for (let i = 0; i < max; i++) {
+		ps.push(newMenu({id: menuIdBase + i + "", title: items[i].title}))
+	}
+	return Promise.all(ps)
 }
 
 function handleMessage(req, sender, sendResponse) {
@@ -178,9 +157,9 @@ function handleMessage(req, sender, sendResponse) {
 	}
 }
 
-function bmDo(name, arg) {
+function bmDo(op, arg) {
 	return new Promise((resolve, reject) => {
-		switch (name) {
+		switch (op) {
 		case "create":
 			chrome.bookmarks.create(arg, x => resolve(x))
 			break
@@ -197,21 +176,42 @@ function bmDo(name, arg) {
 			chrome.bookmarks.remove(arg, x => resolve(x))
 			break
 		default:
-			reject("unknown bookmarks function:" + name)
+			reject("unknown bookmarks function:" + op)
 			return
 		}
 	})
 }
 
-function start() {
-	initBmtab()
-		.then(() => {
-			chrome.contextMenus.onClicked.addListener(handleMenu)
-			chrome.browserAction.onClicked.addListener(captureTab)
-			chrome.runtime.onMessage.addListener(handleMessage)
-
-			buildMenus()
-		})
+function tabDo(op, ...args) {
+	return new Promise((resolve, reject) => {
+		switch (op) {
+		case "create":
+			chrome.tabs.create(args[0], res => resolve(res))
+			break
+		case "query":
+			chrome.tabs.query(args[0], res => resolve(res))
+			break
+		case "remove":
+			chrome.tabs.remove(args[0], () => resolve())
+			break
+		case "update":
+			chrome.tabs.update(args[0], args[1], res => resolve(res))
+			break
+		default:
+			reject("unknown tab function:" + op)
+			break
+		}
+	})
 }
 
-start()
+async function init() {
+	await initBmtab()
+
+	chrome.contextMenus.onClicked.addListener(handleMenu)
+	chrome.browserAction.onClicked.addListener(captureTab)
+	chrome.runtime.onMessage.addListener(handleMessage)
+	
+	buildMenus()
+}
+
+init()
